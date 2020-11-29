a0fd3df08b96047820cbbdbce95a3fd78136fa2e
Vsanjorge <vsanjorgeperez@gmail.com>
2020-11-29 16:57:02 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,13 @@
+/*****
+* Last episode we revisited inputs, prints, conditions, loops and arrays.
+* In this snippet we're introducing OOP (Object Oriented Programming) with classes and objects.
+* We'll try to use concepts from the previous lessons too!
+*****/
+
+import java.util.Scanner; // remember to import our trusty Scanner
+
+class Main {
+  public static void main(String[] args) {
+    // THIS IS A WORK IN PROGRESS!!
+  }
+}
